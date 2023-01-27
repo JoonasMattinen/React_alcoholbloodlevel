fea9cdeb941449deae85cbea3325672a6884f22b
JoonasMattinen <N2majo02.students.oamk.fi>
2023-01-27 13:21:51 +0200

    added form

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <form>
+      <h1>Calculating alcohol blood level</h1>
+      <div>
+        <label>Weight </label>
+        <input type="number" />
+      </div>
+      <div>
+        <label>Bottles </label>
+        <input type="number"></input>
+      </div>
+      <div>
+        <label>time </label>
+        <input type="number" />
+      </div>
+      <div>
+        <input type="radio" />
+        <label>Male</label>
+        <input type="radio" />
+        <label>Female</label>
+      </div>
+      <output></output>
+      <button type="butotn">Calculate</button>
+    </form>
   );
 }
 
